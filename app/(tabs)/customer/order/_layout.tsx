274d38f2e4d00afca490615e6bfb2855f0430a37
sahil sah <sahil2720kumar@gmail.com>
2026-03-01 15:58:11 +0530

    fix cart bug

--- app/(tabs)/customer/order/_layout.tsx
+++ app/(tabs)/customer/order/_layout.tsx
@@ -1,6 +1,5 @@
 import { router, Stack } from 'expo-router';
 import Feather from '@expo/vector-icons/Feather';
-import Ionicons from '@expo/vector-icons/Ionicons';
 import { useCartPriceSync } from "@/hooks/usecartpricesync"
 
 export default function OrderLayout() {
